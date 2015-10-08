1db297346a2170f9abeeffbb73740d87c6acb043
mirtlbu <bubuzoglo@gmail.com>
2015-10-08 17:21:19 +0300

    started landing re-forma

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function() {
+    console.log('init');
+    // var labels = document.getElementsByClassName('label__item'),
+    //     inputs = document.getElementsByClassName('input');
+    // function showInput(e) {
+    //     var input_id = e.target.getAttribute('for');
+    //     Array.prototype.forEach.call(inputs, function(v, i) {
+    //         v.className = 'input';
+    //     });
+    //     document.getElementById(input_id).className = 'input input--active';
+    // }
+    // Array.prototype.forEach.call(labels, function(v, i) {
+    //         v.addEventListener("click", showInput);
+    //     });
+})()
