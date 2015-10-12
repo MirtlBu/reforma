d24c3264d2d90eb49f31dbf8eb449231205a63f4
mirtlbu <bubuzoglo@gmail.com>
2015-10-12 21:03:40 +0300

    almost done!

--- js/script.js
+++ js/script.js
@@ -1,15 +1,69 @@
-(function() {
+$(function() {
     console.log('init');
-    // var labels = document.getElementsByClassName('label__item'),
-    //     inputs = document.getElementsByClassName('input');
-    // function showInput(e) {
-    //     var input_id = e.target.getAttribute('for');
-    //     Array.prototype.forEach.call(inputs, function(v, i) {
-    //         v.className = 'input';
-    //     });
-    //     document.getElementById(input_id).className = 'input input--active';
-    // }
-    // Array.prototype.forEach.call(labels, function(v, i) {
-    //         v.addEventListener("click", showInput);
-    //     });
-})()
+
+    jQuery.validator.setDefaults({
+        debug: true,
+        success: 'valid'
+    });
+
+    jQuery.validator.addMethod("yourname", function (name, element) {
+        return this.optional(element) || name.length > 2 &&
+              name.match(/^[- а-яА-ЯёЁ]+$/);
+              // [A-Z]'?[- a-zA-Z]+$
+    });
+
+    jQuery.validator.addMethod("phone", function (phone_number, element) {
+        phone_number = phone_number.replace(/\s+/g, "");
+        return this.optional(element) || phone_number.length > 9 &&
+              phone_number.match(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/);
+    });
+
+    var form = $('#welcome__form');
+    var formData = $(form).serialize();
+
+    form.validate({
+        errorElement: 'span',
+        rules: {
+            yourname: {
+                required: true,
+                yourname: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true,
+                phone: true
+            }
+        },
+        messages: {
+            yourname: {
+                required: 'Пожалуйста, введите имя',
+                yourname: 'Пожалуйста, введите правильное имя.'
+            },
+            email: {
+                required: 'Пожалуйста, введите email',
+                email: 'Пожалуйста, введите правильный email, типа ivanov@mail.com'
+            },
+            phone: {
+                required: 'Пожалуйста, введите номер телефона',
+                phone: 'Пожалуйста, введите правильный номер телефона.'
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.appendTo(element.parent('.welcome__label'));
+        },
+        submitHandler: function(form) {
+            $(form).ajaxSubmit();
+            $('.welcome__form').addClass('form--invisible');
+            $('.thanks').addClass('thanks--active');
+          }
+    });
+
+    $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    })
+})
---
+++ mailer.php
@@ -0,0 +1,40 @@
+<?php
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $yourname = $_POST['yourname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+
+        $recipient = "bubuworking@gmail.com";
+
+        // Set the email subject.
+        $subject = "мебель на заказ";
+
+        // Build the email content.
+        $email_content = "Имя: $yourname\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Телефон:\n$phone\n";
+
+        // Build the email headers.
+        $email_headers = "From: $yourname <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
+?>
