28f3b4495aafb97c7e511a627f422a35d7d1db1d
mirtlbu <bubuzoglo@gmail.com>
2015-10-18 23:35:33 +0300

    add bottom form, add forms behavior

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ $(function() {
 
     jQuery.validator.addMethod("yourname", function (name, element) {
         return this.optional(element) || name.length > 2 &&
-              name.match(/^[- а-яА-ЯёЁ]+$/);
-              // [A-Z]'?[- a-zA-Z]+$
+              name.match(/^[- а-яА-ЯёЁa-zA-Z]+$/);
     });
 
     jQuery.validator.addMethod("phone", function (phone_number, element) {
@@ -18,52 +17,53 @@ $(function() {
               phone_number.match(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/);
     });
 
-    var form = $('#welcome__form');
-    var formData = $(form).serialize();
+    var forms = $('.form');
 
-    form.validate({
-        errorElement: 'span',
-        rules: {
-            yourname: {
-                required: true,
-                yourname: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            phone: {
-                required: true,
-                phone: true
-            }
-        },
-        messages: {
-            yourname: {
-                required: 'Пожалуйста, введите имя',
-                yourname: 'Пожалуйста, введите правильное имя.'
-            },
-            email: {
-                required: 'Пожалуйста, введите email',
-                email: 'Пожалуйста, введите правильный email, типа ivanov@mail.com'
-            },
-            phone: {
-                required: 'Пожалуйста, введите номер телефона',
-                phone: 'Пожалуйста, введите правильный номер телефона.'
-            }
-        },
-        errorPlacement: function(error, element) {
-            error.appendTo(element.parent('.welcome__label'));
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit();
-            $('.welcome__form').addClass('form--invisible');
-            $('.thanks').addClass('thanks--active');
-          }
-    });
+    setValidation(forms);
 
-    $.ajax({
-        type: 'POST',
-        url: $(form).attr('action'),
-        data: formData
-    })
+    function setValidation(elemsCollection) {
+        Array.prototype.forEach.call(elemsCollection, function(v, i) {
+            $(v).validate({
+                errorElement: 'span',
+                rules: {
+                    yourname: {
+                        required: true,
+                        yourname: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    phone: {
+                        required: true,
+                        phone: true
+                    }
+                },
+                messages: {
+                    yourname: {
+                        required: 'Пожалуйста, введите имя',
+                        yourname: 'Пожалуйста, введите правильное имя.'
+                    },
+                    email: {
+                        required: 'Пожалуйста, введите email',
+                        email: 'Пожалуйста, введите правильный email, типа ivanov@mail.com'
+                    },
+                    phone: {
+                        required: 'Пожалуйста, введите номер телефона',
+                        phone: 'Пожалуйста, введите правильный номер телефона.'
+                    }
+                },
+                errorPlacement: function(error, element) {
+                    // $('.welcome').height('auto');
+                    error.appendTo(element.parent('.label'));
+                },
+                submitHandler: function(form) {
+                    $(form).ajaxSubmit();
+                    $('.form').addClass('form--invisible');
+                    $('.thanks').addClass('thanks--active');
+                }
+            });
+        });
+    }
+
 })
